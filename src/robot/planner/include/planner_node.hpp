821c013ccfc9a8390284b5aae3b151771351c864
divyamakkar0 <divyamakkar000@gmail.com>
2025-01-18 22:10:29 -0500

    module 3 base done

--- src/robot/planner/include/planner_node.hpp
+++ src/robot/planner/include/planner_node.hpp
@@ -2,12 +2,28 @@
 #define PLANNER_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "nav_msgs/msg/path.hpp"
 #include "planner_core.hpp"
 
 class PlannerNode : public rclcpp::Node {
   public:
     PlannerNode();
+
+    void readMap(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    void readGoal(const geometry_msgs::msg::PointStamped::SharedPtr msg);
+    void readOdom(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void publishPath();
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_readings;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped::SharedPtr>::SharedPtr goal_readings; 
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_readings;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pathPub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    std::pair<float,float> goal_point;
+    std::pair<float,float> pos;
+    
 
   private:
     robot::PlannerCore planner_;
--- src/robot/planner/src/planner_node.cpp
+++ src/robot/planner/src/planner_node.cpp
@@ -1,6 +1,36 @@
 #include "planner_node.hpp"
+using std::placeholders::_1;
 
-PlannerNode::PlannerNode() : Node("planner"), planner_(robot::PlannerCore(this->get_logger())) {}
+PlannerNode::PlannerNode() : Node("planner"), planner_(robot::PlannerCore(this->get_logger())) {
+  map_readings = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+    "/map", 10, std::bind(&PlannerNode::readMap, this, _1));
+  goal_readings = this->create_subscription<geometry_msgs::msg::PointStamped>(
+    "/goal_point", 10, std::bind(&PlannerNode::readGoal, this, _1));
+  odom_readings = this->create_subscription<nav_msgs::msg::Odometry>(
+    "/odom/filtered", 10, std::bind(&PlannerNode::readOdom, this, _1));
+  pathPub_ = this->create_publisher<nav_msgs::msg::Path>("/path", 10);
+
+  //TO-DO: bind the timer to function - temp binded to publishPath
+  timer_ = this->create_wall_timer(
+    std::chrono::milliseconds(500), std::bind(&MapMemoryNode::publishPath, this));
+}
+
+//updarte cmake
+void PlannerNode::readMap(const nav_msgs::msg::OccupancyGrid::SharedPtr msg){
+  std::vector<signed char> map = msg->data;
+
+}
+
+void PlannerNode::readGoal(const geometry_msgs::msg::PointStamped::SharedPtr msg){
+  this->goal_point = std::make_pair(msg->point.x, msg->point.y);
+
+}
+
+void PlannerNode::readOdom(const nav_msgs::msg::Odometry::SharedPtr msg){
+  this->pos = std::make_pair(msg->pose.pose.position.x, msg->pose.pose.position.y);
+}
+
+void
 
 int main(int argc, char ** argv)
 {
